b97fcfc8d76bd641c485f35b02e520115f130f56
Dmitri <DRU@popesco.com>
2014-08-15 09:32:54 +0300

    composer update

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -1,18 +1,4 @@
 <?php
 
-function autoload($className)
-{
-	$className = ltrim($className, '\\');
-	$fileName  = '';
-	$namespace = '';
-	if ($lastNsPos = strrpos($className, '\\')) {
-		$namespace = substr($className, 0, $lastNsPos);
-		$className = substr($className, $lastNsPos + 1);
-		$fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-	}
-	$fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-
-	require $fileName;
-}
-
-spl_autoload_register('autoload');
+require_once 'lib/SepaXmlFile.php';
+require_once 'lib/SEPA/Factory/XmlGeneratorFactory.php';
